05d1de9f64a9d54688ad15dda19cf3ae54f56063
Carl Olsen <unstoppablecarlolsen@gmail.com>
2025-09-10 10:17:58 -0400

    deepFreeze.test.ts improvements

--- tests/deepFreeze.test.ts
+++ tests/deepFreeze.test.ts
@@ -1,45 +1,77 @@
 import { describe, expect, it } from 'vitest'
 import { deepFreeze } from '../src/deepFreeze'
+import { recordTypeKey } from '../src'
 
 describe('deepFreeze() unit tests', async () => {
-  it('circular reference', async () => {
+  function func() {
+  }
 
-    function func() {
+  const closure = () => {
+  }
 
-    }
+  const arrItem1 = { id: 1 }
+  const arrItem2 = { id: 2 }
 
-    const closure = () => {}
+  const grandChild = {
+    name: 'jim',
+  }
 
-    const a: { b: object } = { b: {} }
+  type Child = {
+    name: string,
+    sibling?: Child,
+    grandChild?: object,
+  }
+  const child1: Child = {
+    name: 'c1',
+    grandChild,
+  }
 
-    const b = {
-      a,
-    }
+  const child2 = {
+    name: 'c2',
+    sibling: child1,
+  }
 
-    a.b = b
+  child1.sibling = child2
 
-    const arrItem1 = {id: 1}
-    const arrItem2 = {id: 2}
+  const otherRecord = {
+    [recordTypeKey]: 'Thing',
+    child: {
+      foo: 'bar',
+    },
+  }
 
-    const f = {
-      a,
-      func: func,
-      closure,
-      str: 'string',
-      number: 99,
-      decimal: 1.04,
-      array: [arrItem1, arrItem2],
-    }
+  const obj = {
+    func,
+    closure,
+    str: 'string',
+    number: 99,
+    decimal: 1.04,
+    array: [arrItem1, arrItem2],
+    child1,
+    child2,
+    otherRecord,
+  }
+  deepFreeze(obj)
 
-    deepFreeze(f)
-
-    expect(Object.isFrozen(f)).toBe(true)
-    expect(Object.isFrozen(a)).toBe(true)
-    expect(Object.isFrozen(b)).toBe(true)
+  it('freezable types', async () => {
+    expect(Object.isFrozen(obj)).toBe(true)
+    expect(Object.isFrozen(child1)).toBe(true)
+    expect(Object.isFrozen(child2)).toBe(true)
+    expect(Object.isFrozen(grandChild)).toBe(true)
     expect(Object.isFrozen(func)).toBe(true)
     expect(Object.isFrozen(closure)).toBe(true)
     expect(Object.isFrozen(arrItem1)).toBe(true)
     expect(Object.isFrozen(arrItem2)).toBe(true)
+  })
 
+  it('ignores child objects with [recordTypeKey]', async () => {
+    expect(Object.isFrozen(otherRecord)).toBe(false)
+    expect(Object.isFrozen(otherRecord.child)).toBe(false)
+  })
+
+  it('circular reference', async () => {
+    expect(Object.isFrozen(child1)).toBe(true)
+    expect(Object.isFrozen(child2)).toBe(true)
+    expect(Object.isFrozen(child1.grandChild)).toBe(true)
   })
 })
