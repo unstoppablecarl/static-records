6a0c4d46426b60752c4ba3f65b0f053c357d565b
Carl Olsen <unstoppablecarlolsen@gmail.com>
2025-09-13 13:37:51 -0400

    comment improvement

--- src/type-util.ts
+++ src/type-util.ts
@@ -1,4 +1,5 @@
-// set keys of ProtoItem to undefined | never on the Item to prevent overwrites
+// set keys of ProtoItem to undefined | never on the Item
+// to prevent overwrites when filling
 export type NeverProtoKeys<Item, ProtoItem> = Omit<Item, keyof ProtoItem> & {
   [K in keyof ProtoItem]?: never
 }
